b30abce6c81ea65bbfe1c57e434c6d7578a1f7bc
RumbleRum <dnjswls0339@naver.com>
2024-10-11 17:59:21 +0900

    큐브이스케이프 업로드

--- project/0822(큐브이스케이프)/script/index.js
+++ project/0822(큐브이스케이프)/script/index.js
@@ -1,49 +1,26 @@
 
-$('#right-s').click(function () {
-    $('.art1-left').addClass('on')
-    $('.art2.left').addClass('on')
+
+
+const sections = document.querySelectorAll('section')
+
+sections.forEach((sect, i) =>{
+    sect.addEventListener('wheel',function(e){
+        e.preventDefault()
+
+        let delta = e.deltaY
+
+        if(delta < 0 && i === 0){
+            return
+        }else if(delta > 0 && i === sections.length-1){
+            return
+        }
+
+        let secTop = delta < 0 ? sect.previousElementSibling : sect.nextElementSibling
+        if(secTop){
+            let targetSecTop =secTop.offsetTop
+            window.scrollTo({top :targetSecTop, behavior:'smooth'})
+        }
+    })
 })
-
-$('#left-s').click(function () {
-    $('.art1-right').addClass('on')
-    $('.art2.right').addClass('on')
-})
-
-$('#angel').click(function () {
-    $('.art1-end').addClass('on')
-    $('.art2.end').addClass('on')
-})
-
-$(".hidden-text").mCustomScrollbar({
-    theme: "minimal-dark"
-});
-
-$('.hidden-text').on('mouseleave',function(){
-    $(this).mCustomScrollbar('scrollTo','0')
-})
-
-var swiper = new Swiper(".mySwiper", {
-
-    initialSlide: 4, // 인덱스 4(5번째 슬라이드)부터 시작
-    // 추가 설정 옵션들
-    loop: true,
-    slidesPerView: 5,
-    centeredSlides: true,
-    // 기타 옵션들
-
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-
-   
-});
 
 
